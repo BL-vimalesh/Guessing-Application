f6d6704d130295a3113ea3f0d9c3ccf6e5761cbd
vimalesh <vimalesh.ravikumar@bridgelabz.com>
2026-02-06 12:40:03 +0530

    Added GameConfig uc-1 code.

---
+++ src/GuessingApp.java
@@ -0,0 +1,9 @@
+package src;
+
+public class GuessingApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Guess App");
+        GameConfig guess = new GameConfig();
+        guess.show();
+    }
+}
