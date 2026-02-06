f039ae57c704a47db16c317d8ee30210378ce9f4
vimalesh <vimalesh.ravikumar@bridgelabz.com>
2026-02-06 14:15:23 +0530

    Added GuessValidator uc-2 code.

---
+++ src/GuessingApp.java
@@ -0,0 +1,28 @@
+package src;
+
+import java.util.Scanner;
+public class GuessingApp {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Guessing App");
+        GameConfig config = new GameConfig();
+        config.show();
+
+        Scanner sc = new Scanner(System.in);
+        int attempts = 0;
+
+        while (attempts < config.getMAX_ATTEMPTS()) {
+
+            System.out.print("enter your guess");
+
+            int guess = sc.nextInt();
+            attempts++;
+
+            String result = GuessValidator.Validator(guess, config.getTarget_number());
+            System.out.println(result);
+
+            if ("CORRECT".equals(result)) {
+                break;
+            }
+        }
+    }}
